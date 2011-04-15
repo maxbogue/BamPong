432ea43c4eee662ad0ce15c10729dc9e0d866d58
Brian Gernhardt <bcg2784@rit.edu>
2011-04-15 13:58:42 -0400

    Merge branch 'android'
    Conflicts:
    	.classpath
    	.gitignore
    	.project
    	src/bam/pong/BamPong.java

---
+++ src/bam/pong/android/BamPong.java
@@ -0,0 +1,13 @@
+package bam.pong.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BamPong extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
