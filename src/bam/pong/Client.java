3aefa278c6de591570a9d70e0f7e983c6d09cb0f
Brian Gernhardt <bcg2784@rit.edu>
2011-04-21 17:48:38 -0400

    Add socket setup to client code.
    Using NIO to avoid piles of threads and synchronization.
    Channels are thread-safe, as is the map I'm currently going to
    use as a set.

--- src/bam/pong/Client.java
+++ src/bam/pong/Client.java
@@ -1,24 +1,65 @@
 package bam.pong;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.List;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-// Basic outline of client networking
+/**
+ * Handles all client-server and client-client communications.
+ */
 public class Client {
-	public final String SERVER_ADDRESS = "127.0.0.1";
-	public final int    SERVER_PORT    = 1234;
+	public static final String SERVER_ADDRESS = "127.0.0.1";
+	public static final int    SERVER_PORT    = 1234;
 	
-	ServerSocket incoming;
-	Socket server;
-	List<Socket> peers;
+	// Use NIO channels to avoid having a thread per socket.
+	private ServerSocketChannel incoming;
+	private SocketChannel server;
+	private Map<SocketChannel, Client> peers; // We're using a concurrent map as a set.
+	private Thread listener;
+	private Selector selector;
 	
-	Client() {
+	private class Listener implements Runnable {
+		@Override
+		public void run() {
+			// loop while any socket is open
+			// wait on selector
+			// If peer socket, processPeerMessage
+			// If server socket, processServerMessage
+			// If incoming socket, add incoming connection as peer
+		}
+	}
+	
+	
+	/**
+	 * Initializes incoming socket and connects to server.
+	 * 
+	 * @throws IOException for any socket problems
+	 */
+	Client() throws IOException {
+		peers = new ConcurrentHashMap<SocketChannel, Client>();
+
 		// create incoming socket
-		// Send incoming socket port to server
-		// setup callback to processServerMessage
-		// Get list of other clients
-		// Connect to other clients
+		incoming = ServerSocketChannel.open();
+		incoming.socket().bind(new InetSocketAddress(0));
+
+		// connect to server
+		server = SocketChannel.open(new InetSocketAddress(
+				InetAddress.getByName(SERVER_ADDRESS), SERVER_PORT));
+		
+		// create selector for thread
+		selector = Selector.open();
+		// register sockets
+		incoming.register(selector, incoming.validOps());
+		server.  register(selector, server  .validOps());
+		
+		// Start listener thread
+		listener = new Thread(new Listener());
+		listener.run();
 	}
 	
 	void connectToClient(/* IP, port */) {
@@ -42,7 +83,12 @@ public class Client {
 	}
 
 	public static void main(String args[]) {
-		Client c = new Client();
-		c.sendMessageToClients("Hello World!");
+		try {
+			Client c = new Client();
+			c.sendMessageToClients("Hello World!");
+		} catch (IOException e) {
+			System.err.println(e);
+			e.printStackTrace();
+		}
 	}
 }
