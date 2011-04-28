2c564e6ccc2fa2e22dab3692f8b486750c5568f2
aniket <aniket@aniket-PC>
2011-04-28 14:34:35 -0400

    The touch screen stuff is not working,and d code does not support multiple balls.

--- src/bam/pong/android/BamPong.java
+++ src/bam/pong/android/BamPong.java
@@ -1,13 +1,115 @@
 package bam.pong.android;
 
-import android.app.Activity;
-import android.os.Bundle;
+//Author:Aniket
 
-public class BamPong extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    //import bam.pong.Engine;
+    
+     import android.app.Activity;
+   
+    
+    import android.view.KeyEvent;
+    import android.view.MotionEvent;
+    import android.view.View;
+    import android.view.Window;
+    import android.view.View.OnClickListener;
+    import android.view.View.OnKeyListener;
+    import android.view.View.OnTouchListener;
+    
+    import android.os.Bundle;
+    import android.util.DisplayMetrics;
+
+
+
+    public class BamPong extends Activity implements  OnKeyListener, OnTouchListener , OnClickListener {
+        
+           
+           // private Engine En;
+            
+    	private GameField gf;
+            
+         
+        public void onCreate(Bundle state)
+        {
+            super.onCreate(state);
+            
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            
+            DisplayMetrics display = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(display);
+           
+            //Height of display in pixels
+            int ht = display.heightPixels;
+            
+            
+            //Width of Display in pixels
+            int wdt = display.widthPixels;
+           
+            
+            //En=new Engine(Width,Height);        
+            
+            
+            //New gamefield object
+            gf = new GameField(this, wdt, ht);
+            
+            gf.setOnClickListener(this);
+            
+            
+            
+            gf.setOnTouchListener(this);
+            
+            setContentView(gf);
+            
+     
+        }
+
+            
+
+            public boolean onKey(View v, int Code, KeyEvent e) {
+                    
+                    gf.en.moveLeft();
+                    setContentView(gf);
+                    return false;
+            }
+            
+            public boolean onKeyDown(int Code, KeyEvent e)
+            {
+                    
+                    switch(Code) {
+                    //Left movement of arrow key
+                    case KeyEvent.KEYCODE_DPAD_LEFT : 
+                        gf.en.moveLeft();
+                        break;
+                    //Right movement of the arrow key
+                    case KeyEvent.KEYCODE_DPAD_RIGHT : 
+                            gf.en.moveRight();
+                            break;
+                            
+                    
+                            
+                    }
+            
+                    setContentView(gf);
+                    return true;
+            }
+
+            public boolean onTouch(View v,MotionEvent event)
+            {  
+               
+            	
+            	//Do stuff here to make the touch screen work
+            	
+            	return true;
+            }
+            
+            
+            public void onClick(View v) {
+                
+                gf.en.moveLeft();
+                setContentView(gf);
+        }
+            
+
+			
+
     }
-}
+    
---
+++ src/bam/pong/android/GameField.java
@@ -0,0 +1,70 @@
+package bam.pong.android;
+
+
+	import android.content.Context;
+
+	
+	import android.graphics.Canvas;
+	import android.view.View;
+
+
+	public class GameField extends View implements Runnable {
+	        
+		   // public Paddle paddle;
+	       
+	       // private Ball ball;
+      public Engine en;   
+		
+	    private int Width, Height;
+
+	        
+	        public GameField(Context context, int wdt, int ht) {
+	                super(context);
+
+	                this.Width = wdt;
+	                this.Height = ht;
+	                
+	          en=new Engine(context, Width, Height);      
+
+	       // paddle = new Paddle(context, Width, Height);
+	       
+	        
+	        //ball = new Ball(context, paddle, Width, Height);
+	        
+	        
+	                Thread t = new Thread(this);
+	                t.start();
+
+	        }
+	        
+	    protected void onDraw(Canvas canvas)
+	    
+	    {
+	        this.postInvalidate();
+	        
+	        
+	        //paddle.draw(canvas);
+	        
+	        
+	        //ball.draw(canvas);
+	        
+	        en.draw(canvas);
+	    }
+	    
+	        public void run() {
+	                while(true){
+	                        try {
+	                                Thread.sleep(20);
+	                        } catch (InterruptedException e) {
+	                                // TODO Auto-generated catch block
+	                                e.printStackTrace();
+	                        }
+	                        en.move();
+	                        //ball2.move();
+	                }
+	        }
+	        
+
+	}
+
+
