20c0e5e9daa891def99ca4544bef4448675d3086
Brian Gernhardt <bcg2784@rit.edu>
2011-05-11 12:36:57 -0400

    Comment Constants

--- src/bam/pong/Constants.java
+++ src/bam/pong/Constants.java
@@ -1,12 +1,18 @@
 package bam.pong;
 
+/** Constants needed by multple classes */
 public class Constants {
 
+	/** Client-Server: List Games */
 	public static final byte LIST_GAMES = 1;
+	/** Client-Server: Create Game */
 	public static final byte CREATE_GAME = 2;
+	/** Client-Server: Cancel Game */
 	public static final byte CANCEL_GAME = 3;
+	/** Client-Server: Join Game */
 	public static final byte JOIN_GAME = 4;
+	/** Client-Server: Start Game */
 	public static final byte START_GAME = 5;
+	/** Server to Client: Game Canceled */
 	public static final byte GAME_CANCELED = 6;
-	
 }
